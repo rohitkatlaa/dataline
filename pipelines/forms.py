97129118d1be10a686cf6d72b8608b48deb859c0
rohitkatlaa <rohitkatlaa@gmail.com>
2022-03-18 16:01:39 +0530

    removing stuff

--- pipelines/forms.py
+++ pipelines/forms.py
@@ -1,20 +1,6 @@
-from attr import field
 from django import forms
 
 from pipelines.models import Pipeline, Operation
-
-# class PipelineForm(forms.Form):
-#   name = forms.CharField()
-
-#   def clean(self):
-#     data = self.cleaned_data
-#     name = data.get("name")
-
-#     # Has to limited to a particular user and not global
-#     qs = Pipeline.objects.filter(name__icontains=name)
-#     if qs.exists():
-#       self.add_error("name", f"{name} is already taken. Please pick another name.")
-#     return data
 
 class PipelineForm(forms.ModelForm):
   error_css_class = 'error-field'
