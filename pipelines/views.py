b081662010e88af42fae1cfcfdea36901f0ddd45
rohitkatlaa <rohitkatlaa@gmail.com>
2022-04-07 16:04:22 +0530

    adding pipelien execution

--- pipelines/views.py
+++ pipelines/views.py
@@ -1,9 +1,15 @@
+from urllib import response
 from django.shortcuts import get_object_or_404, render, redirect
+from django.http import FileResponse
 from django.forms.models import modelformset_factory
+from dataline.settings import BASE_DIR
 from pipelines.forms import OperationForm, PipelineForm
 from pipelines.models import Pipeline, Operation
 from django.contrib.auth.decorators import login_required
-
+from django.core.files.storage import FileSystemStorage
+import pipelines.pipeline_execution.data as pdata
+import pipelines.pipeline_execution.pipeline_structure as ps
+import os
 
 def home_view(request):
   context = {
@@ -32,6 +38,61 @@ def pipeline_detail_view(request, id=None):
   }
 
   return render(request, "pipeline/detail.html", context)
+
+
+@login_required
+def pipeline_execute_view(request, id=None):
+  pipeline_obj = get_object_or_404(Pipeline, id=id, user=request.user)
+  context = {
+    "object": pipeline_obj
+  }
+  if request.method == 'POST' and request.FILES['data_file']:
+    data_file = request.FILES['data_file']
+    fs = FileSystemStorage()
+    filename = fs.save(data_file.name, data_file)
+    uploaded_file_url = fs.url(filename)
+    return redirect(pipeline_execution_output_view, id, filename)
+  return render(request, "pipeline/execute.html", context)
+
+@login_required
+def pipeline_execution_output_view(request, id=None, filename=None):
+  pipeline_obj = get_object_or_404(Pipeline, id=id, user=request.user)
+
+  input_data_dict = pdata.DataReader.generate_csv_data_dict(os.path.join(BASE_DIR, 'media', filename), filename)
+
+  p_format = ps.PipelineFormat()
+  stage_dict = {}
+  for operation in pipeline_obj.get_operation_children():
+    if operation.stage_name not in stage_dict.keys():
+      s_format = ps.StageFormat(operation.stage_name)
+      stage_dict[operation.stage_name] = s_format
+    
+    t_format = ps.TaskFormat([operation.data_input_name], operation.operation_name, operation.parameters, operation.data_output_name)
+
+    stage_dict[operation.stage_name].add_task_format(t_format)
+
+
+  for stage_name in stage_dict:
+    p_format.add_stage_format(stage_dict[stage_name])
+
+  pipeline_instance = ps.Pipeline(pipeline_obj.name, input_data_dict, p_format)
+  output = pipeline_instance.execute()
+  final_output = output.fetch_dt(operation.data_output_name).fetch_table()
+  final_output.to_csv("media/"+pipeline_instance.name+"_output.csv")
+  os.remove("media/"+filename)
+
+  context = {
+    "object": pipeline_obj,
+    "output": pipeline_instance.name+"_output.csv"
+  }
+  return render(request, "pipeline/execution_output.html", context)
+
+
+@login_required
+def send_output_file(request, name):
+  output = open('media/'+name, 'rb')
+  response = FileResponse(output)
+  return response
 
 
 @login_required
