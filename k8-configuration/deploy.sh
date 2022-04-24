ebb04c6990e36d2d09f649384f9466261dae6d7d
rohitkatlaa <rohitkatlaa@gmail.com>
2022-04-24 11:18:10 +0530

    adding k8 configs

---
+++ k8-configuration/deploy.sh
@@ -0,0 +1,14 @@
+# cd ../pipeline_creation
+# docker build -t rohitkatlaa/dataline_pipeline_creation:latest .
+# cd ../pipeline_execution
+# docker build -t rohitkatlaa/dataline_pipeline_execution:latest .
+# docker push rohitkatlaa/dataline_pipeline_creation:latest
+# docker push rohitkatlaa/dataline_pipeline_execution:latest
+# cd ../k8-configuration
+kubectl apply -f dataline-namespace.yaml
+kubectl apply -f postgres-secret.yaml --namespace=dataline
+kubectl apply -f postgres.yaml --namespace=dataline
+kubectl apply -f postgres-configmap.yaml --namespace=dataline
+kubectl apply -f pipeline_creation.yaml --namespace=dataline
+kubectl apply -f pipeline_execution.yaml --namespace=dataline
+kubectl apply -f dataline_ingress.yaml --namespace=dataline
