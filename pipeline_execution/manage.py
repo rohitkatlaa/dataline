dbc4611437f19f82fa3b8e7243cccce61706d2a8
rohitkatlaa <rohitkatlaa@gmail.com>
2022-05-06 18:53:24 +0530

    adding test settings file

--- pipeline_execution/manage.py
+++ pipeline_execution/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pipeline_execution.settings')
+    if 'test' in sys.argv:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pipeline_execution.test_settings')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pipeline_execution.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
